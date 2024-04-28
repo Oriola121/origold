356efd02b3955a150d3b610a82d95b311ad4016d
Muhibudeen AbdurRahman Oriola <muhibudeen.oriola@gmail.com>
2024-04-28 23:11:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from './Main'
+import GlobalStyle from './GlobalStyle'
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Main() {
+  return (
+    <Maincont>
+        <p>turning ideas into<br/>real life products<br/> is my callings</p>
+    </Maincont>
+  )
+}
+
+const Maincont = styled.div`
+width: 100%;
+height: 100vh;
+background: #10101A;
+color: #fff;
+`
